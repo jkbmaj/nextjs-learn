8987a4c283b351a19db9406a3499a2cfa37c9947
Jakub Maj <jakub@jkbmaj.io>
2023-09-11 00:05:42 +0200

    Test (#3)
    * Some changes
    
    * Add a list of 4 products
    
    * Change routing

---
+++ src/app/products/page.tsx
@@ -0,0 +1,17 @@
+import { ProductList } from "@/app/organisms/ProductList";
+import type { ProductItemType } from "@/app/ProductItemType";
+
+export default function Products() {
+	const products: ProductItemType[] = [
+		{ id: 1, name: "Koszulka", src: "#1", imageUrl: "/img1.jpeg" },
+		{ id: 2, name: "Buty", src: "#2", imageUrl: "/img2.jpeg" },
+		{ id: 3, name: "Spodnie", src: "#3", imageUrl: "/img3.jpeg" },
+		{ id: 4, name: "Plecak", src: "#4", imageUrl: "/img4.jpeg" },
+	];
+
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<ProductList products={products} />
+		</main>
+	);
+}
