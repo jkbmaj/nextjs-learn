f2d5337d103555471c0d2e3e2984618bcdcb1c79
Jakub Maj <jakub@jkbmaj.io>
2023-09-11 00:07:19 +0200

    Remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import { ProductList } from "@/app/organisms/ProductList";
-import type { ProductItemType } from "@/app/ProductItemType";
-
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
